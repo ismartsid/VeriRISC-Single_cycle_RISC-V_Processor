40f2b96dcd5c4943d3450f90ae207f8092d219b6
Sidhartha <85611307+ismartsid@users.noreply.github.com>
2024-01-29 12:17:25 -0700

    Update fetch.sv

--- fetch.sv
+++ fetch.sv
@@ -15,8 +15,6 @@ module fetch (
   output   logic [31:0]   instr_mem_instr_o
 );
 
-  // Write your logic here...
-
   always_ff @(posedge clk or negedge reset_n)
     begin
       if (reset_n == 0)
