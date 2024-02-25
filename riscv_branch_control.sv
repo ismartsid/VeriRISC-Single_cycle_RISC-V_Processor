fe3f13ca61be2f2c9836ed9db19bf6e1525474ea
Sidhartha <85611307+ismartsid@users.noreply.github.com>
2024-02-25 00:14:05 -0700

    Added comments

--- riscv_branch_control.sv
+++ riscv_branch_control.sv
@@ -1,28 +1,32 @@
 module riscv_branch_control import riscv_pkg::*; (
-  // Source operands
-  input  logic [31:0] opr_a_i,
-  input  logic [31:0] opr_b_i,
+  
+  input  logic [31:0] opr_a_i, //RS1
+  input  logic [31:0] opr_b_i, //RS2
 
-  // Branch Type
-  input  logic        is_b_type_ctl_i,
-  input  logic [2:0]  instr_func3_ctl_i,
-
-  // Branch outcome
-  output logic        branch_taken_o
+  input  logic        is_b_type_ctl_i,  //Input from decode unit specifying weather it is a B_type instruction or not.
+  input  logic [2:0]  instr_func3_ctl_i, //funct3 Field for specifying the type of branch operation
+  
+  output logic        branch_taken_o //Output for a branch to be taken or not
 );
 
+  logic [31:0] twos_comp_a;
+  logic [31:0] twos_comp_b;
+  
+  assign twos_comp_a = opr_a_i[31] ? ~opr_a_i + 32'h1 : opr_a_i; //two's complement of RS1
+  assign twos_comp_b = opr_b_i[31] ? ~opr_b_i + 32'h1 : opr_b_i; //two's complement of RS2
+  
 always_comb
   begin
     if(is_b_type_ctl_i)
       begin
     case(instr_func3_ctl_i)
-      BEQ: branch_taken_o = (opr_a_i == opr_b_i);
-      BNE: branch_taken_o = (opr_a_i != opr_b_i);
-      BLT: branch_taken_o = (opr_a_i < opr_b_i);
-      BGE: branch_taken_o = (opr_a_i >= opr_b_i);
-      BLTU: branch_taken_o = ($signed(opr_a_i) < $signed(opr_b_i));
-      BGEU: branch_taken_o = (opr_a_i >= opr_b_i);
-      default: branch_taken_o = 1'b0;
+      BEQ: branch_taken_o = (opr_a_i == opr_b_i); //Branch if RS1 = RS2
+      BNE: branch_taken_o = (opr_a_i != opr_b_i); //Branch if RS1 != RS2
+      BLT: branch_taken_o = (opr_a_i < opr_b_i); //Branch if RS1 < RS2
+      BGE: branch_taken_o = (opr_a_i >= opr_b_i); //Branch if RS1 >= RS2
+      BLTU: branch_taken_o = (twos_comp_a <  twos_comp_b); //Branch if RS1 < RS2 (Unsigned)
+      BGEU: branch_taken_o = (twos_comp_a >= twos_comp_b); //Branch if RS1 >= RS2 (Unsigned)
+      default: branch_taken_o = 1'b0; //Branch not taken
     endcase
       end
       else
