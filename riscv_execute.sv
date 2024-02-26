49617a4be39b46cbc1907acaf22a76a58bcdebb1
Sidhartha <85611307+ismartsid@users.noreply.github.com>
2024-02-25 19:11:58 -0700

    added comments

--- riscv_execute.sv
+++ riscv_execute.sv
@@ -13,9 +13,9 @@ module riscv_execute import riscv_pkg::*; (
   		case(op_sel_i)
       			ADD_op: alu_res_o = opr_a_i + opr_b_i; //ADD RS1 + RS2
 			SUB_op: alu_res_o = opr_a_i - opr_b_i; //SUB RS1 - RS2
-			SLL_op: alu_res_o = opr_a_i << opr_b_i[4:0]; //LEFT SHIFT RS1 by the value specified in RS2
-			SRL_op: alu_res_o = opr_a_i >> opr_b_i[4:0]; //RIGHT SHIFT RS1 by the value specified in RS2
-			SRA_op: alu_res_o = $signed(opr_a_i) >>> opr_b_i[4:0]; //RIGHT SHIFT RS1 by the value specified in RS2 (Signed)
+			SLL_op: alu_res_o = opr_a_i << opr_b_i[4:0]; //LOGICAL LEFT SHIFT RS1 by the value specified in RS2
+			SRL_op: alu_res_o = opr_a_i >> opr_b_i[4:0]; //LOGICAL RIGHT SHIFT RS1 by the value specified in RS2
+			SRA_op: alu_res_o = $signed(opr_a_i) >>> opr_b_i[4:0]; //ARTHEMATIC RIGHT SHIFT RS1 by the value specified in RS2
 			OR_op: alu_res_o = opr_a_i | opr_b_i; //OR RS1 | RS2
 			AND_op: alu_res_o = opr_a_i & opr_b_i; //AND RS1 & RS2
 			XOR_op: alu_res_o = opr_a_i ^ opr_b_i; //XOR RS1 ^ RS2
